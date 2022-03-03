f888f1a51e0c4976b8523fc314f7d277a0a2b9a0
Pablo Cuadrado <pablocuadrado@gmail.com>
2022-03-03 09:27:04 -0300

    Basic mockup.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/orkes-logo-purple.png" height="100" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          UI Question
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className='articleList'>
+        <div className='articleCard'>
+          <div className='articlePicture'>
+            <img alt="Placeholder pic." src="https://picsum.photos/250/250" />
+          </div>
+          <div className='articleContent'>
+            <h1>Title lorem ipsum.</h1>
+            <p className='articleDate'>Dec 02, 2021 03:51 AM IST</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
