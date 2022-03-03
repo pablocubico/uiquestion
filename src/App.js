431419a4fdf509504c6427cb36df8135598b393a
Pablo Cuadrado <pablocuadrado@gmail.com>
2022-03-03 09:47:11 -0300

    First working prototype.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ page, setPage ] = useState(1);
+  const [ articles, setArticles ] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchArticles = async () => {
+      const articlesResponse = await fetch(`https://www.pinkvilla.com/photo-gallery-feed-page/page/${page}`)
+      const newArticles = await articlesResponse.json();
+
+      setIsLoading(false);
+      setArticles(previous => [
+        ...previous,
+        // Flattening a bit for convenience
+        ...newArticles.nodes.map(article => article.node)
+      ]);
+    }
+    fetchArticles();
+  }, [page])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +31,24 @@ function App() {
         </p>
       </header>
       <div className='articleList'>
-        <div className='articleCard'>
-          <div className='articlePicture'>
-            <img alt="Placeholder pic." src="https://picsum.photos/250/250" />
-          </div>
-          <div className='articleContent'>
-            <h1>Title lorem ipsum.</h1>
-            <p className='articleDate'>Dec 02, 2021 03:51 AM IST</p>
-          </div>
-        </div>
+        {
+          articles.map((article, i) => {
+            return (
+              <div className='articleCard' key={i}>
+                <div className='articlePicture'>
+                  <img alt="Placeholder pic." src={ `https://www.pinkvilla.com/${article.field_photo_image_section}` } />
+                </div>
+                <div className='articleContent'>
+                  <h1>{ article.title }</h1>
+                  <p className='articleDate'>Dec 02, 2021 03:51 AM IST</p>
+                </div>
+              </div>
+            )
+          })
+        }
+        {
+          isLoading && <div className='loader'>Loading...</div>
+        }
       </div>
     </div>
   );
